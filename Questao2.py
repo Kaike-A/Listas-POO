00caa724e306c3ec3215b90a5bfec9ed15cd43b2
Kaike <douglasqueto@gmail.com>
2026-08-31 18:51:02 -0300

    Questão 2

---
+++ Questao2.py
@@ -0,0 +1,7 @@
+tempodeslocamento = float(input('Tempo de deslocamento (em horas): '))
+
+segundos = tempodeslocamento * 3600
+distancia = 30 * segundos
+metros = distancia * 1000
+
+print(f'Distância percorrida em metros: {metros:.2f}')
