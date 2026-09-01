dbcb1b4a03a1355b62ced694302f7a517614bbb7
Kaike <douglasqueto@gmail.com>
2026-08-31 21:56:47 -0300

    Questão 5

---
+++ Questao5.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite o último número da sua placa: "))
+
+if numero % 2 == 0:
+    print("Número par, diriga-se ao portão A")
+else:
+    print("Número ímpar, diriga-se ao portão B")
