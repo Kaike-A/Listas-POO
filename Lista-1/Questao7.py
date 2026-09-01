d7125bfb7129ee6c6b43d8f462a5ab784d1af615
Kaike <douglasqueto@gmail.com>
2026-09-01 00:03:54 -0300

    Questão 7

---
+++ Lista-1/Questao7.py
@@ -0,0 +1,6 @@
+total_arquivos = 5776
+
+if total_arquivos % 3 == 0:
+    print("Os arquivos podem ser distribuídos igualmente entre os grupos, sem sobras.")
+else:
+    print(f"Não é possível distribuir igualmente. Sobrará arquivos.")
