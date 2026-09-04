20baa0ed11d17ac0fc3a00b249d9bcabdfafd7df
Kaike <douglasqueto@gmail.com>
2026-09-04 16:49:52 -0300

    QUESTAO 35 RESPONDIDA EM VIDEO

--- Lista-3/Questao35.py
+++ Lista-3/Questao35.py
@@ -8,3 +8,34 @@
 # - Excluir um equipamento do cadastro;
 # - Identificar e exibir o equipamento que possui o maior custo estimado de reparo.
 
+equipamentos = {}
+
+while True:
+    print('\n1.Cadastrar \n2.Listar \n3.Atualizar \n4.Excluir \n5.Mais caro \n6.Sair\n')
+    opcao = input('Escolha uma opção: ')
+
+    if opcao == '1':
+        nome = input('Nome do equipamento: ')
+        custo = float(input('Custo do reparo: R$ '))
+        equipamentos[nome] = custo
+
+    elif opcao == '2':
+        for nome, custo in equipamentos.items():
+            print(f'- {nome}: R$ {custo}')
+
+    elif opcao == '3':
+        nome = input('Qual o equipamento deseja atualizar? ')
+        if nome in equipamentos:
+            equipamentos[nome] = float(input('Novo custo: R$ '))
+
+    elif opcao == '4':
+        nome = input('Qual equipamento deseja excluir? ')
+        if nome in equipamentos:
+            del equipamentos[nome]
+
+    elif opcao == '5':
+        mais_caro = max(equipamentos, key=equipamentos.get)
+        print(f'O mais caro é {mais_caro} custando R$ {equipamentos[mais_caro]}')
+
+    elif opcao == '6':
+        break
